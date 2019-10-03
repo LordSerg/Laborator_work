5c50dcf7a5ef6a59087fc5ad34a151ebeae0e72c
Moskalenko Sergey <oknelaksoms@gmail.com>
2019-10-04 00:02:04 +0300

    ЛБ1

---
+++ Hellow/Hellow/Hellow.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "HELLO!!!\n";
+	system("pause");
+	return 0;
+}
---
+++ Square/Square/Square.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	cout << " _________________\n";
+	cout << "|                 |\n";
+    cout << "|                 |\n";
+	cout << "|                 |\n";
+	cout << "|                 |\n";
+	cout << "|                 |\n";
+	cout << "|                 |\n";
+	cout << "|                 |\n";
+	cout << "|_________________|\n";
+	system("pause");
+	return 0;
+}
